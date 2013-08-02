e790af699995da498d0d9e8efded9489b8ccc129
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2013-08-02 13:13:46 +0200

    mode-line customization

--- themes/color-theme-tronesque.el
+++ themes/color-theme-tronesque.el
@@ -26,7 +26,7 @@
 
  `(button ((t (:background ,magenta :foreground ,base00 :box (:line-width 2 :style released-button)))))
  `(button ((t (:background ,magenta :foreground ,base00 :box (:line-width 2 :style released-button)))))
- `(cursor ((t (:inverse-video t :foreground ,base00 :background ,base04))))
+ `(cursor ((t (:background ,base06))))
  `(custom-button ((t (:background ,magenta :foreground ,base00 :box (:line-width 2 :style released-button)))))
  `(custom-state ((t (:foreground ,green))))
  `(default ((t (:background ,base00 :foreground ,base04))))
@@ -50,7 +50,7 @@
  `(font-lock-warning-face ((t (:foreground ,orange :underline t))))
  `(fringe ((t (:foreground ,base00 :background ,base06))))
  `(header-line ((t (:box (:line-width -1 :color nil :style released-button) :foreground ,base00 :background ,base04))))
- `(highlight ((t (:background ,yellow))))
+ `(highlight ((t (:background ,yellow :foreground ,base00))))
  `(hl-line ((t (:background ,base01))))
  `(idle-highlight ((t (:background ,base01 :foreground nil))))
  `(ido-indicator ((t (:background ,red :foreground ,base00 :width condensed))))
@@ -65,11 +65,6 @@
  `(link-visited ((t (:foreground ,blue :underline t))))
  `(match ((t (:foreground ,base00 :background ,blue))))
  `(minibuffer-prompt ((t (:foreground ,yellow))))
- `(mode-line ((t (:box (:line-width -1 :color nil :style nil) :foreground ,base04 :background ,base00))))
- `(mode-line-buffer-id ((t (:weight normal :foreground ,base02 :background ,base04))))
- `(mode-line-emphasis ((t (:weight bold))))
- `(mode-line-highlight ((t :background ,magenta :foreground ,base00 (:box (:line-width 2 :style released-button)))))
- `(mode-line-inactive ((t (:box (:line-width -1 :color nil :style nil) :foreground ,base00 :background ,base04))))
  `(next-error ((t (:inherit (region)))))
  `(org-hide ((t (:background ,base00 :foreground ,base00))))
  `(query-replace ((t (:inherit (isearch)))))
@@ -112,9 +107,12 @@
  `(erc-input-face ((t (:foreground ,orange))))
  `(erc-my-nick-face ((t (:foreground ,yellow))))
  `(erc-current-nick-face ((t (:foreground ,yellow))))
- `
- `(sp-show-pair-match-face ((t (:background ,base03 :foreground ,base04))))
- `(sp-show-pair-mismatch-face ((t (:background ,red :foreground ,base00))))
+
+ `(show-paren-match ((t (:background ,green :foreground ,base00))))
+ `(show-paren-mismatch ((t (:background ,red :foreground ,base00))))
+ `(sp-show-pair-match-face ((t (:inherit show-paren-match))))
+ `(sp-show-pair-mismatch-face ((t (:inherit show-paren-mismatch))))
+ `(sp-pair-overlay-face ((t (:inherit sp-show-pair-match-face))))
 
  )
 
@@ -128,6 +126,139 @@
    ;; black, red, green, yellow, blue, magenta, cyan, white
    [unspecified ,base00 ,red ,green ,yellow ,blue ,magenta ,cyan ,base04]))
 
+
+(setq-default
+ mode-line-format
+ '(; Position, including warning for 80 columns
+   (:propertize "%4l:" face mode-line-position-face)
+   (:eval (propertize "%3c" 'face
+                      (if (>= (current-column) 80)
+                          'mode-line-80col-face
+                        'mode-line-position-face)))
+                                        ; emacsclient [default -- keep?]
+   mode-line-client
+   " "
+                                        ; read-only or modified status
+   (:eval
+    (cond (buffer-read-only
+           (propertize " RO " 'face 'mode-line-read-only-face))
+          ((buffer-modified-p)
+           (propertize " ** " 'face 'mode-line-modified-face))
+          (t (propertize " == " 'face 'mode-line-clean-face))))
+   (:propertize " "
+                face mode-line-folder-face)
+                                        ; directory and
+                                        ; buffer/file name
+
+   (:propertize (:eval (shorten-directory default-directory 30))
+                face mode-line-folder-face)
+   (:propertize "%b "
+                face mode-line-filename-face)
+                                        ; mode indicators: vc, recursive edit, major mode, minor modes, process, global
+   ;; (vc-mode vc-mode)
+
+   (:propertize " "
+                face mode-line-mode-face)
+   (:propertize mode-name
+                face mode-line-mode-face)
+   (:propertize " "
+                face mode-line-mode-face)
+
+   (:eval (propertize (format-mode-line minor-mode-alist)
+                      'face 'mode-line-minor-mode-face))
+                                        ; narrow [default -- keep?]
+   ;; " %n "
+
+   (:propertize mode-line-process
+                face mode-line-process-face)
+   (global-mode-string global-mode-string)
+   "    "
+                                        ; nyan-mode uses nyan cat as an alternative to %p
+   (:eval (when nyan-mode (list (nyan-create))))
+   ))
+
+;; Helper function
+(defun shorten-directory (dir max-length)
+  "Show up to `max-length' characters of a directory name `dir'."
+  (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
+        (output ""))
+    (when (and path (equal "" (car path)))
+      (setq path (cdr path)))
+    (while (and path (< (length output) (- max-length 4)))
+      (setq output (concat (car path) "/" output))
+      (setq path (cdr path)))
+    (when path
+      (setq output (concat ".../" output)))
+    output))
+
+;; Extra mode line faces
+
+(make-face 'mode-line-read-only-face)
+(make-face 'mode-line-modified-face)
+(make-face 'mode-line-clean-face)
+(make-face 'mode-line-folder-face)
+(make-face 'mode-line-filename-face)
+(make-face 'mode-line-position-face)
+(make-face 'mode-line-mode-face)
+(make-face 'mode-line-minor-mode-face)
+(make-face 'mode-line-process-face)
+(make-face 'mode-line-80col-face)
+
+(set-face-attribute 'mode-line nil
+                    :foreground "#081724" :background "#2872b2"
+                    :inverse-video nil
+                    :box '(:line-width 6 :color "#2872b2"  :style nil))
+(set-face-attribute 'mode-line-inactive nil
+                    :foreground "#081724" :background "#1d5483"
+                    :inverse-video nil
+                    :box '(:line-width 6 :color "#1d5483" :style nil))
+
+(set-face-attribute 'mode-line-read-only-face nil
+                    :inherit 'mode-line-face
+                    :foreground "#ff694d"
+                    :box '(:line-width 2 :color "#ff694d"))
+(set-face-attribute 'mode-line-modified-face nil
+                    :inherit 'mode-line-face
+                    :background "#ff694d"
+                    :box '(:line-width 6 :color "#ff694d" :style nil))
+(set-face-attribute 'mode-line-clean-face nil
+                    :inherit 'mode-line-face
+                    :background "#68f6cb"
+                    :box '(:line-width 6 :color "#68f6cb" :style nil))
+(set-face-attribute 'mode-line-folder-face nil
+                    :inherit 'mode-line-face
+                    :foreground "#081724"
+                    :background "#96a5d9"
+                    :box '(:line-width 6 :color "#96a5d9" :style nil)
+                    )
+(set-face-attribute 'mode-line-filename-face nil
+                    :inherit 'mode-line-face
+                    :foreground "#1d5483"
+                    :background "#96a5d9"
+                    :box '(:line-width 6 :color "#96a5d9" :style nil)
+                    :weight 'bold)
+(set-face-attribute 'mode-line-position-face nil
+                    :inherit 'mode-line-face
+                    :height 100)
+(set-face-attribute 'mode-line-mode-face nil
+                    :inherit 'mode-line-face
+                    :foreground "#081724"
+                    :background "#f5b55f"
+                    :box '(:line-width 6 :color "#f5b55f" :style nil)
+                    )
+(set-face-attribute 'mode-line-minor-mode-face nil
+                    :foreground "#081724"
+                    :height 110)
+(set-face-attribute 'mode-line-process-face nil
+                    :inherit 'mode-line-face
+                    :foreground "#68f6cb")
+(set-face-attribute 'mode-line-80col-face nil
+                    :inherit 'mode-line-position-face
+                    :foreground "black" :background "#eab700"
+                    :box '(:line-width 6 :color "#eab700" :style nil)
+                    )
 )
+
+
 
 (provide-theme 'tronesque)
